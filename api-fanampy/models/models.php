ff52f40966354791c53e26aa30097315b27ecc04
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-03-09 20:32:59 +0300

    Models pour la connexion à la base de données...

---
+++ api-fanampy/models/models.php
@@ -0,0 +1,30 @@
+<?php
+abstract class Database {
+    private string $host;
+    private string $database;
+    private string $user;
+    private string $password;
+
+    public function __construct() {
+        $lahatra=json_decode(file_get_contents('./db.json'));
+        $this->host = $lahatra->host;
+        $this->database = $lahatra->database;
+        $this->user = $lahatra->user;
+        $this->password = $lahatra->password;
+    }
+
+    protected function db_connect():object {
+        try {
+            return new PDO("mysql:host=$this->host; dbname=$this->database; charset=utf8", 
+                $this->user, $this->password,
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        catch(Exception $e) {
+            print_r(json_encode([
+                'status' => false,
+                'message' => "Erreur: Connexion à la base de données non établie !".$e->getMessage()
+            ], JSON_FORCE_OBJECT));
+        }
+    }
+}
+
