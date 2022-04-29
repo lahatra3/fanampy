7f3f352b3084ef6ae9097c38ce616d11f0b222e0
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-04-29 19:46:21 +0300

    [ADD] intercepteurs, services...

--- fanampy/src/app/core/core.module.ts
+++ fanampy/src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
+import { httpInterceptorProviders } from './interceptors';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [
+    httpInterceptorProviders
   ]
 })
 export class CoreModule { }
--- fanampy/src/app/core/services/auth.service.ts
+++ fanampy/src/app/core/services/auth.service.ts
@@ -1,6 +1,14 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class AuthService {
     
+    constructor(private http: HttpClient) { }
+    
+    getToken(): string | null {
+        return localStorage.getItem('fanampy_lahatra3');
+    }
 }
